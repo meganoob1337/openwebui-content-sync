6e592da34faea9b4755bfd7ed1743eac4e7fc379
Meganoob1337 <an3wilkens@gmail.com>
2025-09-29 11:10:17 +0000

    fix typo

--- internal/config/config.go
+++ internal/config/config.go
@@ -76,7 +76,7 @@ type ConfluenceConfig struct {
 	ParentPageMappings []ParentPageMapping `yaml:"parent_page_mappings"` // Per-parent-page knowledge mappings
 	PageLimit          int                 `yaml:"page_limit"`
 	IncludeAttachments bool                `yaml:"include_attachments"`
-	UseMarkdownParser  bool                `yaml:"use_mardown_parser"`
+	UseMarkdownParser  bool                `yaml:"use_markdown_parser"`
 	IncludeBlogPosts   bool                `yaml:"include_blog_posts"`
 }
 
